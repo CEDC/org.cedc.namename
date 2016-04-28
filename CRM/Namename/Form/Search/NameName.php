760c91cc5d1c52721d6f2531637fb59f7166c281
Laryn Bakker <laryn@Laryns-iMac.local>
2016-04-27 22:19:36 -0500

    Initial commit after creating with civix and tweaking

---
+++ CRM/Namename/Form/Search/NameName.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * A custom contact search
+ */
+class CRM_Namename_Form_Search_NameName extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+  function __construct(&$formValues) {
+    parent::__construct($formValues);
+  }
+
+  /**
+   * Prepare a set of search fields
+   *
+   * @param CRM_Core_Form $form modifiable
+   * @return void
+   */
+  function buildForm(&$form) {
+    CRM_Utils_System::setTitle(ts('Search For Spam Entries Where First Name = Last Name'));
+
+  }
+
+  /**
+   * Get a list of summary data points
+   *
+   * @return mixed; NULL or array with keys:
+   *  - summary: string
+   *  - total: numeric
+   */
+  function summary() {
+    return NULL;
+    // return array(
+    //   'summary' => 'This is a summary',
+    //   'total' => 50.0,
+    // );
+  }
+
+  /**
+   * Get a list of displayable columns
+   *
+   * @return array, keys are printable column headers and values are SQL column names
+   */
+  function &columns() {
+    // return by reference
+    $columns = array(
+      ts('First Name') => 'first_name',
+      ts('Last Name') => 'last_name',
+      ts('Contact Id') => 'contact_id',
+    );
+    return $columns;
+  }
+
+  /**
+   * Construct a full SQL query which returns one page worth of results
+   *
+   * @param int $offset
+   * @param int $rowcount
+   * @param null $sort
+   * @param bool $includeContactIDs
+   * @param bool $justIDs
+   * @return string, sql
+   */
+  function all($offset = 0, $rowcount = 0, $sort = NULL, $includeContactIDs = FALSE, $justIDs = FALSE) {
+    // delegate to $this->sql(), $this->select(), $this->from(), $this->where(), etc.
+    return $this->sql($this->select(), $offset, $rowcount, $sort, $includeContactIDs, NULL);
+  }
+
+  /**
+   * Construct a SQL SELECT clause
+   *
+   * @return string, sql fragment with SELECT arguments
+   */
+  function select() {
+    return "
+      contact_a.`id` as contact_id,
+      contact_a.`first_name` as first_name,
+      contact_a.`last_name` as last_name
+    ";
+  }
+
+  /**
+   * Construct a SQL FROM clause
+   *
+   * @return string, sql fragment with FROM and JOIN clauses
+   */
+  function from() {
+    return "
+      FROM `civicrm_contact` as contact_a
+    ";
+  }
+
+  /**
+   * Construct a SQL WHERE clause
+   *
+   * @param bool $includeContactIDs
+   * @return string, sql fragment with conditional expressions
+   */
+  function where($includeContactIDs = FALSE) {
+    $params = array();
+    $where = " ( contact_a.`contact_type` = 'Individual' ) AND ( contact_a.first_name = contact_a.last_name ) ";
+
+    return $this->whereClause($where, $params);
+  }
+
+  /**
+   * Determine the Smarty template for the search screen
+   *
+   * @return string, template path (findable through Smarty template path)
+   */
+  function templateFile() {
+    return 'CRM/Contact/Form/Search/Custom.tpl';
+  }
+
+  /**
+   * Modify the content of each row
+   *
+   * @param array $row modifiable SQL result row
+   * @return void
+   */
+  function alterRow(&$row) {
+    //$row['sort_name'] .= ' ( altered )';
+  }
+}
